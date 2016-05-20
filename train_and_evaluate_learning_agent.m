c2a7295e434af90094a25d1126e92a085bfcb989
E.Valldor <erik.valldor@gmail.com>
2016-05-20 15:19:53 +0200

    ..

--- train_and_evaluate_learning_agent.m
+++ train_and_evaluate_learning_agent.m
@@ -2,49 +2,80 @@ global charDatasetPath;
 global lpDatasetPath;
 
 % Set these to the correct locations
-charDatasetPath = '~/Pictures/char_dataset_18.5/';
+charDatasetPath = '~/Pictures/char_dataset_20.5/';
 lpDatasetPath = '~/Pictures/lp_dataset/';
 
 
 % variable 'lp_data' is loaded with information about all images
 load([lpDatasetPath '/lp_data']);
 
-
+% Input height and width
 height = 30;
 width = 15;
 
-
-
 %Extract validation data from the image dataset
-[validationInputs, validationTargets, inputLicensePlateIds] = extract_character_regions_from_all_images(lp_data, height, width);
-
+disp(['Extracting inputs and targets from license plate data set...']);
+tic
+[extractedInputs, extractedTargets, extractedLicensePlateIds] = extract_character_regions_from_all_images(lp_data, height, width);
+toc
 
 % Construct some training data
+disp(['Creating training data...']);
 tic
-[inputs, targets] = create_training_data(height, width);
+[trainInputs, trainTargets] = create_training_data(height, width);
 toc
 
 %view_training_data(inputs, height, width, 1);
 
 
 
-% Train a neural network on the data
-net = patternnet(40);
-net = init(net);
-net.divideFcn = 'dividetrain';
-%net.trainParam.max_fail = 20;
+numberOfTimesToTestNetwork = 5;
+performance = zeros(1, numberOfTimesToTestNetwork);
+stability = zeros(1, numberOfTimesToTestNetwork);
+for time = 1:numberOfTimesToTestNetwork
+    
+    disp(sprintf('Creating and training neural network... (%d/%d)', time, numberOfTimesToTestNetwork));
+    uniqueLicensePlates = unique(extractedLicensePlateIds);
+    
+    randomIndices = randperm(size(uniqueLicensePlates, 1));
+        
+    validationIndices = find(ismember(extractedLicensePlateIds, ...
+                                      uniqueLicensePlates(randomIndices(1:floor(length(randomIndices)/2)))));
+    testIndices = find(ismember(extractedLicensePlateIds, ...
+                                      uniqueLicensePlates(randomIndices(floor(length(randomIndices)/2)+1:length(randomIndices)))));
+    
+    
+    validationInputs = extractedInputs(validationIndices, :);
+    validationTargets = extractedTargets(validationIndices, :);
 
-trained_net = train(net, inputs', targets');
-%genFunction(trained_net, 'neural_char_recognition');
+    testInputs = extractedInputs(testIndices, :);
+    testTargets = extractedTargets(testIndices, :);
+        
+    % Train a neural network on the data
+    net = patternnet(40);
+    net = init(net);
+    
+    net.divideFcn = 'divideind';
+    net.divideParam.trainInd = 1:size(trainInputs, 1);
+    net.divideParam.valInd = size(trainInputs, 1)+1:size(trainInputs, 1)+size(validationInputs, 1);
+    net.divideParam.testInd = size(trainInputs, 1)+size(validationInputs, 1)+1:size(trainInputs, 1)+size(validationInputs, 1)+size(testInputs, 1);
+    
+    %net.trainParam.max_fail = 20;
 
+    trained_net = train(net, [trainInputs' validationInputs' testInputs'], [trainTargets' validationTargets' testTargets']);
 
+    % Check network performance
+    testOutputs = sim(trained_net,  extractedInputs(testIndices, :)');
+    %    [c confMat ind per] = confusion(extractedTargets', testOutputs);
+    %    imagesc(confMat);
 
-% Check network performance
-validationOutputs = sim(trained_net,  validationInputs');
-[c confMat ind per] = confusion(validationTargets', validationOutputs);
-imagesc(confMat);
+    [numRegions, numCorrectRegions, numLicensePlates, numCorrectPlates] = check_network_performance(testInputs, testOutputs', testTargets, extractedLicensePlateIds(testIndices));
 
-[numRegions, numCorrectRegions, numLicensePlates, numCorrectPlates] = check_network_performance(validationInputs, validationOutputs', validationTargets, inputLicensePlateIds);
+    performance(time) = numCorrectRegions/numRegions;
+    stability(time) = numCorrectPlates/numLicensePlates;
+end
+performance
+stability
+disp(sprintf('Average Performance over %d runs: %f', numberOfTimesToTestNetwork, mean(performance(:))));
+disp(sprintf('Average Stability over %d runs: %f', numberOfTimesToTestNetwork, mean(stability(:))));
 
-disp(sprintf('Regions: %d/%d (%f)', numCorrectRegions, numRegions, numCorrectRegions/numRegions));
-disp(sprintf('Plates: %d/%d (%f)', numCorrectPlates, numLicensePlates, numCorrectPlates/numLicensePlates));
